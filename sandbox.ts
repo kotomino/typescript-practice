be52325a4d2a31877297e7c009f257748fe4cd17
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-05-25 15:35:53 -0500

    explicit types

--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,25 @@
-//arrays
-let names = ['luigi', 'mario', 'yoshi'];
+let ninjas : string[] = [];
+ninjas.push('kotomi');
 
-names.push('toad');
-// names.push(3);
-// names[0] = 3;
-// names = "string"
+let mixed: (string|number|boolean)[] = [];
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+console.log(mixed);
 
-let numbers = [10, 20, 30, 40];
-
-numbers.push(25);
-// numbers.push('ralph');
-// numbers[0]= 'ralph'
-
-let mixed = ['ken', 4, 'chun-li', 8, 9];
-
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+let uid: string|number;
+uid = '123';
+uid = 123;
 
 //objects
-let ninja = {
-  name: 'mario',
-  belt: 'black',
-  age: 30
-};
+let ninjaOne: object;
+ninjaOne = { name: 'yoshi', age: 30 };
 
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking'];
 
-ninja = {
-  name: 'yoshi',
-  belt: 'orange',
-  age: 40
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColour: string
 }
+
+ninjaTwo = { name: 'mario', age: 20, beltColour: 'black' }
